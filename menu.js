99c6aec77bd2abe2212821de1be066547a09219e
Carlos Gonzalez <carlosngonzalez0@gmail.com>
2022-08-03 15:19:15 -0300

    Se modifica el JS para los box y se trabaja con el renderizado del detalle del producto

--- menu.js
+++ menu.js
@@ -1,5 +1,8 @@
-
 const boxContainer = document.querySelector('.box');
+const productDetailContainer = document.querySelector('#productDetail');
+const productDetailCloseIcon = document.querySelector('.product-detail-close');
+const beerDetail = document.querySelector('#beerDetail');
+const productDetail = document.querySelector('#productDetail');
 
 $(document).ready(main);
 
@@ -25,7 +28,15 @@ function main(){
 
 };
 
+// productDetailCloseIcon.addEventListener('click', closeProductDetailAside);
 
+// function openProductDetailAside() {
+//     productDetailContainer.classList.remove('inactive');
+// }
+
+// function closeProductDetailAside() {
+//     productDetailContainer.classList.add('inactive');
+// }
 
 const beerList = [];
 beerList.push({
@@ -90,6 +101,7 @@ function renderBeer(arr) {
 
 		const beerImg = document.createElement('img');
 		beerImg.setAttribute('src', beer.image);
+        // beerImg.addEventListener('click', openProductDetailAside);
 
 		const beerName = document.createElement('h2');
 		beerName.innerText = beer.name;
@@ -115,7 +127,7 @@ function renderBeer(arr) {
 
 renderBeer(beerList);
 
-{/* <div id="beerDetail" class="section__container--box">
+/* <div id="beerDetail" class="section__container--box">
         <div class="box">
             <picture>
                 <source media="(min-width:1000px)" srcset="./img/beer.jpg">
@@ -139,4 +151,64 @@ renderBeer(beerList);
             </p>
             <a href="#"">Saber mas</a>
         </div>
-    </div> */}
+    </div> */
+
+	function renderDetailBeer(arr) {
+		for (detail of arr) {
+			const productDetailClosed = document.createElement('div');
+			productDetailClosed.classList.add('productDetailClosed');
+
+			const closedImg = document.createElement('img');
+			closedImg.setAttribute('src', './img/icon_close.png');
+			closedImg.setAttribute('alt', 'close');
+
+			const detailImg = document.createElement('img');
+			detailImg.setAttribute('src', detail.image);
+
+			const productInfo = document.createElement('div');
+			productInfo.classList.add('product-info');
+
+			const productPrice = document.createElement('p');
+			productPrice.innerText = '$' + detail.price;
+			const productName = document.createElement('p');
+			productName.innerText = detail.name;
+			const productDescription = document.createElement('p');
+			productDescription.innerText = detail.description;
+
+			const detailButton = document.createElement('button');
+			detailButton.classList.add('primary-button');
+
+			const cartImg = document.createElement('img');
+			cartImg.setAttribute('src', './img/bt_add_to_cart.svg');
+			cartImg.setAttribute('alt', 'add to cart');
+
+			productInfo.appendChild(productPrice);
+			productInfo.appendChild(productName);
+			productInfo.appendChild(productDescription);
+			productInfo.appendChild(detailButton);
+			productInfo.appendChild(cartImg);
+
+			productDetailClosed.appendChild(closedImg);
+
+			productDetail.appendChild(productInfo);
+			productDetail.appendChild(detailImg);
+			productDetail.appendChild(productDetailClosed);
+		}
+	}
+
+	renderDetailBeer(beerList);
+	// <aside id="productDetail" class="inactive">
+    //         <div class="product-detail-close">
+    //             <img src="./img/icon_close.png" alt="close">
+    //         </div>
+    //         <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="bike">
+    //         <div class="product-info">
+    //             <p>$35,00</p>
+    //             <p>Bike</p>
+    //             <p>With its practical position, this bike also fulfills a decorative function, add your hall or workspace.</p>
+    //             <button class="primary-button add-to-cart-button">
+    //             <img src="./img/bt_add_to_cart.svg" alt="add to cart">
+    //                 Add to cart
+    //             </button>
+    //         </div>
+    //     </aside>
